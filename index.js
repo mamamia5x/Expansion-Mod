23267b2aa0e14e7552ddd3ae345426e508aa4392
Payton Dorger <DORGPAY@kmsd.edu>
2020-11-17 15:27:19 -0600

    (6.17.11.20) Testing Events

--- index.js
+++ index.js
@@ -12,17 +12,28 @@ GDT.addPlatform({
 	genreWeightings : [0.9, 0.6, 1, 0.9, 1, 0.7],
 	audienceWeightings : [0.9, 1, 0.8],
 	techLevel : 4,
-	iconUri : icon,
-	events : [{
-			id : 'big bag o chips',
-			date : '1/1/1',
-			getNotification : function (company) {
-				return new Notification({
-					header : "Industry News".localize(),
-					text : "Coming out of nowhere a company called Greenheart Games has announced that it will publish a new game console called the Greenheart One {0}.".localize().format(General.getETADescription('1/5/1', '4/7/2')),
-					image : icon
-				});
-			}
-		}
-	]
+	iconUri : icon
+});
+GDT.addEvent({
+  id: 12412512525125,
+  date: '1/1/4'
+  getNotification: function (company) {
+		var game = company.currentGame;
+
+		var msg = "It seems that kids in the neighbourhood have started chatting about your upcoming game {0}. Rumour has it, that Billy, your neighbours kid, snuck into the garage and spied on some of the design papers.{n}How he managed to do this is a mystery. You could swear you were sitting in the garage the entire time!\nHow do you want to react?\n\nYou could talk to the parents to get him punished, ignore the incident or maybe invite some of the neighbours over to show them more of the game."
+			.localize().format(game.title);
+		//notice how we break up the story in two screens by using {n}. This creates a better flow than having one longer block of text.
+		//Also, since this is a story with multiple options and the buttons can only hold a small amount of text, we explain the options beforehand.
+
+		//the chatting among kids, creates a bit of hype.
+		//since the event isn't delayed we can do this directly on the company, otherwise we could call adjustHype() on the notification object to schedule the effect with the notification.
+		company.adjustHype(5 + 10 * company.getRandom());//increase hype between 5 and 15.
+
+		return new Notification({
+			sourceId: eventId,//this is important, otherwise nothing will happen when the player selects an option.
+			header: "Billy, the kid".localize(),//granted, this is a silly header.
+			text: msg,
+			options: ["Talk to parents", "Ignore incident", "Invite over"]//maximum of three choices
+		});
+  }
 });
